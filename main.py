ed72623099cea94374d262e5ab1dceda581e6d55
AnastasiaRomanenko <152871654+AnastasiaRomanenko@users.noreply.github.com>
2025-08-01 16:11:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from app import App
 from welcome.welcome_form import WelcomeForm
 
-from welcome.login.customer.choose_restaurant.show_menu.show_customer_order.confirm_order.confirm_order import ConfirmOrder
 def main():
     app = App()
     app.show_frame(WelcomeForm)
